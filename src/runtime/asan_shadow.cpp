a4cddc7dde9c122eb9beeb5975ce1e99c44b0c2f
Adit0507 <adi05072002@gmail.com>
2026-01-18 12:45:56 +0530

    checking if the shadow memory is accesible

--- src/runtime/asan_shadow.cpp
+++ src/runtime/asan_shadow.cpp
@@ -44,14 +44,53 @@ namespace miniasan
     initialized_ = true;
   }
 
-  int8_t * ShadowMemory:: getShadowAddress(uintptr_t addr) {
+  int8_t *ShadowMemory::getShadowAddress(uintptr_t addr)
+  {
     uintptr_t shadow_offset = addr >> SHADOW_SCALE;
-  
+
     return shadow_base_ + shadow_offset;
   }
 
-  int8_t ShadowMemory::getShadowValue(uintptr_t addr) {
+  int8_t ShadowMemory::getShadowValue(uintptr_t addr)
+  {
     return *getShadowAddress(addr);
   }
 
+  bool ShadowMemory ::isAccessible(uintptr_t addr, size_t size)
+  {
+    uintptr_t end_addr = addr + size;
+
+    uintptr_t aligned_start = addr & ~(SHADOW_GRANULARITY - 1);
+    uintptr_t aligned_end = (end_addr + SHADOW_GRANULARITY - 1) & ~(SHADOW_GRANULARITY - 1);
+
+    for (uintptr_t a = aligned_start; a < aligned_end; a += SHADOW_GRANULARITY)
+    {
+      int8_t *shadow = getShadowAddress(a);
+      int8_t shadow_value = *shadow;
+
+      if (shadow_value < 0)
+      {
+        return false;
+      }
+      if (shadow_value > 0)
+      {
+        uintptr_t region_start = a;
+        uintptr_t region_end = a + SHADOW_GRANULARITY;
+
+        if (addr >= region_start && addr < region_start + shadow_value)
+        {
+          if (end_addr > region_start + shadow_value)
+          {
+            return false;
+          }
+        }
+        else if (addr >= region_start + shadow_value)
+        {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
 }
