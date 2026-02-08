c4d47672dea6fe00af85d4bd198e0be75b89dac5
Adit0507 <adi05072002@gmail.com>
2026-02-08 20:27:27 +0530

    method to protect stack variable

--- src/runtime/asan_stack.cpp
+++ src/runtime/asan_stack.cpp
@@ -17,7 +17,24 @@ namespace miniasan
         ShadowMemory::getInstance().poison((uintptr_t)addr, size, kAsanStackRedzoneMagic);
     }
 
-    void StackProtector:: unpoisonStackRedZone(void *addr, size_t size){
-        ShadowMemory:: getInstance().unpoison((uintptr_t)addr, size);
+    void StackProtector::unpoisonStackRedZone(void *addr, size_t size)
+    {
+        ShadowMemory::getInstance().unpoison((uintptr_t)addr, size);
     }
+
+    void StackProtector::protectStackVariable(void *addr, size_t size, void **left_rz, void **right_rz)
+    {
+        uintptr_t var_addr = (uintptr_t)addr;
+        uintptr_t left_rz_addr = var_addr - kStackRedZoneSize;
+        uintptr_t right_rz_addr = var_addr + size;
+
+        *left_rz= (void *)left_rz_addr;
+        *right_rz= (void *)right_rz_addr;
+
+        poisonStackRedZone(*left_rz, kStackRedZoneSize);
+        poisonStackRedZone(*right_rz, kStackRedZoneSize);
+    
+        ShadowMemory:: getInstance().unpoison(var_addr, size);
+    }
+
 }
