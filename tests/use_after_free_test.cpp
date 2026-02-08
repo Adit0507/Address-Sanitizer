023e5f00f937bfe7d7d2a57c1a5beed6a9a6aa56
Adit0507 <adi05072002@gmail.com>
2026-02-08 11:08:11 +0530

    finishing up the use after free test

--- tests/use_after_free_test.cpp
+++ tests/use_after_free_test.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-int main(){
-    std:: cout << "Testing use after free";
+int main()
+{
+    std::cout << "Testing use after free";
 
-    int *ptr= new int(42);
-    std:: cout << "Allocated and initialized: "<< *ptr<< "\n";
+    int *ptr = new int(42);
+    std::cout << "Allocated and initialized: " << *ptr << "\n";
 
+    delete ptr;
+    std::cout << "Freed the pointer\n";
+
+    std::cout << "Attempting to use freed memory..\n";
+    *ptr = 100;
     return 0;
 }
