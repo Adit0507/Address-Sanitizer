00c029e7300540db835b4e8a56b68942c728cd33
Adit0507 <adi05072002@gmail.com>
2026-03-01 11:13:19 +0530

    stack overflow test

---
+++ tests/stack_overflow_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include "../src/runtime/asan_stack.h"
+#include "../src/runtime/asan_interface.h"
+
+void vulnerable_function() {
+    char buffer[10];
+
+    char left_redzone[0];
+    char right_redzone[0];
+    __asan_poison_memory_region(left_redzone, 8);
+    __asan_poison_memory_region(right_redzone, 8);
+
+    std:: cout << "Attempting stack overflow..\n";
+    for(int i= 0; i< 20; i++){ //will trigger an error
+        buffer[i]= 'A'; //overflow
+    }
+
+    __asan_unpoison_memory_region(left_redzone, 8);
+    __asan_unpoison_memory_region(right_redzone, 8);
+}
