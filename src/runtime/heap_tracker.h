b4afa820349d8a51007b12f92f12d6b398822ca8
Adit0507 <adi05072002@gmail.com>
2026-04-01 13:48:17 +0530

    allocation-deallocation hooks

--- src/runtime/heap_tracker.h
+++ src/runtime/heap_tracker.h
@@ -18,6 +18,12 @@ class HeapTracker {
     HeapTracker(const HeapTracker&) = delete;
     HeapTracker& operator =(const HeapTracker&) = delete;
 
+    // allocation-deallocation hooks
+    void *on_malloc(size_t size); //alocates space for redzones, poison/upoisoning user region
+    void *on_calloc(size_t count, size_t elem_size);    //zero initailises user region after on_malloc
+    void *on_realloc(void *ptr, size_t new_size);
+
+    void on_free(void *ptr);//looks up metadtaa, validates pointer, records free stack trace
 };
 
 
