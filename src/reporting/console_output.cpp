4ea53b8f95a45f1b83388cbc3f604de0536bc159
Adit0507 <adi05072002@gmail.com>
2026-04-14 22:12:28 +0530

    console output

---
+++ src/reporting/console_output.cpp
@@ -0,0 +1,87 @@
+#include "console_output.h"
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+
+static constexpr WORD kColorDefault = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+static constexpr WORD kColorRed = FOREGROUND_RED;
+static constexpr WORD kColorBrightRed = FOREGROUND_RED | FOREGROUND_INTENSITY;
+static constexpr WORD kColorYellow = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+static constexpr WORD kColorGreen = FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+static constexpr WORD kColorCyan = FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+static constexpr WORD kColorWhite = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+static constexpr WORD kColorGray = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+
+static HANDLE stderr_handle()
+{
+    static HANDLE h = GetStdHandle(STD_ERROR_HANDLE);
+    return h;
+}
+static bool is_console()
+{
+    DWORD mode = 0;
+
+    return GetConsoleMode(stderr_handle(), &mode) != 0;
+}
+
+void set_color(ConsoleColor color)
+{
+    if (!is_console)
+        return;
+
+    WORD attr = kColorDefault;
+    switch (color)
+    {
+    case ConsoleColor::Red:
+        attr = kColorRed;
+        break;
+
+    case ConsoleColor::BrightRed:
+        attr = kColorBrightRed;
+        break;
+    case ConsoleColor::Yellow:
+        attr = kColorYellow;
+        break;
+    case ConsoleColor::Green:
+        attr = kColorGreen;
+        break;
+    case ConsoleColor::Cyan:
+        attr = kColorCyan;
+        break;
+    case ConsoleColor::White:
+        attr = kColorWhite;
+        break;
+    case ConsoleColor::Gray:
+        attr = kColorGray;
+        break;
+    default:
+        attr = kColorDefault;
+        break;
+    }
+
+    SetConsoleTextAttribute(stderr_handle(), attr);
+}
+
+void reset_color()
+{
+    set_color(ConsoleColor::Default);
+}
+
+void cprint(ConsoleColor color, const char *fmt, ...)
+{
+    set_color(color);
+
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+
+    reset_color();
+}
+
+void print_asan_header(const char *error_kind)
+{
+    fprintf(stderr, "\n");
+    cprint(ConsoleColor::BrightRed, "asan: ERROR: %s\n", error_kind);
+}
---
+++ src/reporting/console_output.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef ASAN_CONSOLE_OUTPUT_H
+#define ASAN_CONSOLE_OUTPUT_H
+
+#include <cstdio>
+
+enum class ConsoleColor {
+    Default,
+    Red,
+    BrightRed,
+    Yellow,
+    Green,
+    Cyan,
+    White,
+    Gray,
+};
+
+void set_color(ConsoleColor color);
+ 
+void reset_color();
+ 
+void cprint(ConsoleColor color, const char* fmt, ...);
+ 
+void print_asan_header(const char* error_kind);
+ 
+#endif
