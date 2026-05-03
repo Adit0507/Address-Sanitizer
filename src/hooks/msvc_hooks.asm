46cda89d0b6f951e473813e035a8e7485c76eb7c
Adit0507 <adi05072002@gmail.com>
2026-05-03 15:16:44 +0530

    x64 MASM hooks for function entry/exit instrumentation

---
+++ src/hooks/msvc_hooks.asm
@@ -0,0 +1,55 @@
+EXTERN penter_handler:PROC ;
+EXTERN pexit_handler:PROC ;
+
+_TEXT SEGMENT
+
+PUBLIC _penter
+_penter PROC
+    ; Saving volatile GP registers
+    push rax
+    push rcx
+    push rdx
+    push r9
+    push r10
+    push r11
+    pushfq
+
+    ;aligning stack at 16bytes before the call
+    ;pushed 7regs(8 bytes each) +pushfq = 64bytes plus the original return address already on the stack =72 bytes   
+
+    sub rsp, 8
+    sub rsp, 32
+    call penter_handler 
+
+    add rsp, 40
+
+    popfq
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdx
+    pop rcx
+    pop rax
+
+    ret 
+_penter EDP
+
+
+PUBLIC _pexit
+_pexit PROC 
+    push rax
+    push rcx
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+    push rax
+
+    ret
+_pexit ENDP
+
+
+_TEXT ENDS
+END
